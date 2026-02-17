1ee863cb52d7154ada50b1d1c83d2016f6b40698
DFC9977 <office@middas.ro>
2026-02-17 16:21:16 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyAtAxHsqXRUMQa2pP2473lIng3NwP9lL-I",
   authDomain: "gosbiromania.firebaseapp.com",
   projectId: "gosbiromania",
-  storageBucket: "gosbiromania.firebasestorage.app",
+  storageBucket: "gosbiromania.appspot.com",
   messagingSenderId: "885972653346",
-  appId: "1:885972653346:web:fa22097b6ba4ef5432017b"
+  appId: "1:885972653346:web:76fda6a5435050e432017b"
 };
