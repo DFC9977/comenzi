fbef3eb5acb1398a1f59c7aebd0c27fd48eb3d9b
DFC9977 <office@middas.ro>
2026-02-22 21:41:50 +0200

    separate firebase prod/staging

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,38 @@
-import { FIREBASE_CONFIG } from "./config.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-functions.js";
+const hostname = window.location.hostname;
 
-export const app = initializeApp(FIREBASE_CONFIG);
+// PROD
+const firebaseConfigProd = {
+  apiKey: "AIzaSyCyuJfFO-9ZiZd_xhrVPzeHumxacDiOHbk",
+  authDomain: "comenzi-2969b.firebaseapp.com",
+  projectId: "comenzi-2969b",
+  storageBucket: "comenzi-2969b.firebasestorage.app",
+  messagingSenderId: "470205004865",
+  appId: "1:470205004865:web:f67d84006aac09396a5ac0"
+};
+
+// STAGING (lasă config-ul vechi gosbiromania)
+const firebaseConfigStaging = {
+  apiKey: "AICI_PUI_CONFIG_GOSBIROMANIA",
+  authDomain: "gosbiromania.firebaseapp.com",
+  projectId: "gosbiromania",
+  storageBucket: "gosbiromania.firebasestorage.app",
+  messagingSenderId: "885972653346",
+  appId: "1:885972653346:web:76fda6a5435050e432017b"
+};
+
+const firebaseConfig =
+  hostname.includes("comenzi-2969b") ||
+  hostname.includes("comenzi9")
+    ? firebaseConfigProd
+    : firebaseConfigStaging;
+
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app, "europe-west1");
+export const storage = getStorage(app);
